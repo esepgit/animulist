4862c06b74740166e23544a743bcfa4172168d1f
esepgit <150302936+esepgit@users.noreply.github.com>
2024-02-12 09:53:49 -0300

    styles improved add icon

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -3,11 +3,11 @@ import { animes as initialAnimes } from "../constants/initialAnimes"
 function AnimeList({ animes=initialAnimes.data, showAnime }) {
   console.log(animes)
   return (
-    <section className="pt-14 mx-4 grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-4 gap-y-10">
+    <section className="justify-items-center pt-14 mx-4 grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-4 gap-y-10">
       {animes.map(anime => (
-        <article key={anime.mal_id} onClick={() => showAnime(anime)} className="relative overflow-hidden cursor-pointer border-2 hover:border.slate-200 border-transparent shadow-slate-400/10 shadow-lg capitalize text-center bg-white rounded-[30px] font-semibold pb-4 group">
+        <article key={anime.mal_id} onClick={() => showAnime(anime)} className="relative max-w-[225px] overflow-hidden cursor-pointer border-2 hover:border.slate-200 border-transparent shadow-slate-400/10 shadow-lg capitalize text-center bg-white rounded-[30px] font-semibold pb-4 group">
           <header className="max-h-[256px]">
-            <img src={anime.images.webp.image_url} className="w-full object-cover group-hover:scale-110 transition-transform" />
+            <img src={anime.images.webp.image_url} className=" group-hover:scale-110 transition-transform" />
           </header>
           <span className="absolute bottom-0 left-0 w-full bg-white">
             {anime.title}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
+import LogoComponent from "./LogoComponent"
 
 function Navbar() {
   return (
-    <header className="bg-black text-white text-4xl text-center py-4">
-      Animulist
+    <header className="bg-black text-white text-2xl text-center py-4 flex justify-center items-center gap-2">
+      <LogoComponent fill="#fff" width={24} height={24} /> ANIMULIST
     </header>
   )
 }
